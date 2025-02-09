05053a2cb21950bc4a27a6282e842de7cea8155b
Pornchai Toonkaew <mangcom@hotmail.com>
2025-02-09 17:45:06 +0700

    Add files via upload

---
+++ config/radius.sql
@@ -0,0 +1,342 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1deb3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Feb 09, 2025 at 04:12 AM
+-- Server version: 10.11.8-MariaDB-0ubuntu0.24.04.1
+-- PHP Version: 8.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `radius`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_users`
+--
+
+CREATE TABLE `admin_users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `passwords` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin_users`
+--
+
+INSERT INTO `admin_users` (`id`, `username`, `passwords`) VALUES
+(1, 'admin', 'radius');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nas`
+--
+
+CREATE TABLE `nas` (
+  `id` int(10) NOT NULL,
+  `nasname` varchar(128) NOT NULL,
+  `shortname` varchar(32) DEFAULT NULL,
+  `type` varchar(30) DEFAULT 'other',
+  `ports` int(5) DEFAULT NULL,
+  `secret` varchar(60) NOT NULL DEFAULT 'secret',
+  `server` varchar(64) DEFAULT NULL,
+  `community` varchar(50) DEFAULT NULL,
+  `description` varchar(200) DEFAULT 'RADIUS Client'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nasreload`
+--
+
+CREATE TABLE `nasreload` (
+  `nasipaddress` varchar(15) NOT NULL,
+  `reloadtime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `radacct`
+--
+
+CREATE TABLE `radacct` (
+  `radacctid` bigint(21) NOT NULL,
+  `acctsessionid` varchar(64) NOT NULL DEFAULT '',
+  `acctuniqueid` varchar(32) NOT NULL DEFAULT '',
+  `username` varchar(64) NOT NULL DEFAULT '',
+  `realm` varchar(64) DEFAULT '',
+  `nasipaddress` varchar(15) NOT NULL DEFAULT '',
+  `nasportid` varchar(32) DEFAULT NULL,
+  `nasporttype` varchar(32) DEFAULT NULL,
+  `acctstarttime` datetime DEFAULT NULL,
+  `acctupdatetime` datetime DEFAULT NULL,
+  `acctstoptime` datetime DEFAULT NULL,
+  `acctinterval` int(12) DEFAULT NULL,
+  `acctsessiontime` int(12) UNSIGNED DEFAULT NULL,
+  `acctauthentic` varchar(32) DEFAULT NULL,
+  `connectinfo_start` varchar(128) DEFAULT NULL,
+  `connectinfo_stop` varchar(128) DEFAULT NULL,
+  `acctinputoctets` bigint(20) DEFAULT NULL,
+  `acctoutputoctets` bigint(20) DEFAULT NULL,
+  `calledstationid` varchar(50) NOT NULL DEFAULT '',
+  `callingstationid` varchar(50) NOT NULL DEFAULT '',
+  `acctterminatecause` varchar(32) NOT NULL DEFAULT '',
+  `servicetype` varchar(32) DEFAULT NULL,
+  `framedprotocol` varchar(32) DEFAULT NULL,
+  `framedipaddress` varchar(15) NOT NULL DEFAULT '',
+  `framedipv6address` varchar(45) NOT NULL DEFAULT '',
+  `framedipv6prefix` varchar(45) NOT NULL DEFAULT '',
+  `framedinterfaceid` varchar(44) NOT NULL DEFAULT '',
+  `delegatedipv6prefix` varchar(45) NOT NULL DEFAULT '',
+  `class` varchar(64) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `radcheck`
+--
+
+CREATE TABLE `radcheck` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `username` varchar(64) NOT NULL DEFAULT '',
+  `attribute` varchar(64) NOT NULL DEFAULT 'Cleartext-Password',
+  `op` char(2) NOT NULL DEFAULT ':=',
+  `value` varchar(253) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `radgroupcheck`
+--
+
+CREATE TABLE `radgroupcheck` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `groupname` varchar(64) NOT NULL DEFAULT '',
+  `attribute` varchar(64) NOT NULL DEFAULT '',
+  `op` char(2) NOT NULL DEFAULT '==',
+  `value` varchar(253) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `radgroupreply`
+--
+
+CREATE TABLE `radgroupreply` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `groupname` varchar(64) NOT NULL DEFAULT '',
+  `attribute` varchar(64) NOT NULL DEFAULT '',
+  `op` char(2) NOT NULL DEFAULT '=',
+  `value` varchar(253) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `radpostauth`
+--
+
+CREATE TABLE `radpostauth` (
+  `id` int(11) NOT NULL,
+  `username` varchar(64) NOT NULL DEFAULT '',
+  `pass` varchar(64) NOT NULL DEFAULT '',
+  `reply` varchar(32) NOT NULL DEFAULT '',
+  `authdate` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6),
+  `class` varchar(64) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `radreply`
+--
+
+CREATE TABLE `radreply` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `username` varchar(64) NOT NULL DEFAULT '',
+  `attribute` varchar(64) NOT NULL DEFAULT '',
+  `op` char(2) NOT NULL DEFAULT '=',
+  `value` varchar(253) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `radusergroup`
+--
+
+CREATE TABLE `radusergroup` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `username` varchar(64) NOT NULL DEFAULT '',
+  `groupname` varchar(64) NOT NULL DEFAULT '',
+  `priority` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin_users`
+--
+ALTER TABLE `admin_users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `nas`
+--
+ALTER TABLE `nas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `nasname` (`nasname`);
+
+--
+-- Indexes for table `nasreload`
+--
+ALTER TABLE `nasreload`
+  ADD PRIMARY KEY (`nasipaddress`);
+
+--
+-- Indexes for table `radacct`
+--
+ALTER TABLE `radacct`
+  ADD PRIMARY KEY (`radacctid`),
+  ADD UNIQUE KEY `acctuniqueid` (`acctuniqueid`),
+  ADD KEY `username` (`username`),
+  ADD KEY `framedipaddress` (`framedipaddress`),
+  ADD KEY `framedipv6address` (`framedipv6address`),
+  ADD KEY `framedipv6prefix` (`framedipv6prefix`),
+  ADD KEY `framedinterfaceid` (`framedinterfaceid`),
+  ADD KEY `delegatedipv6prefix` (`delegatedipv6prefix`),
+  ADD KEY `acctsessionid` (`acctsessionid`),
+  ADD KEY `acctsessiontime` (`acctsessiontime`),
+  ADD KEY `acctstarttime` (`acctstarttime`),
+  ADD KEY `acctinterval` (`acctinterval`),
+  ADD KEY `acctstoptime` (`acctstoptime`),
+  ADD KEY `nasipaddress` (`nasipaddress`),
+  ADD KEY `class` (`class`);
+
+--
+-- Indexes for table `radcheck`
+--
+ALTER TABLE `radcheck`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `username` (`username`(32));
+
+--
+-- Indexes for table `radgroupcheck`
+--
+ALTER TABLE `radgroupcheck`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `groupname` (`groupname`(32));
+
+--
+-- Indexes for table `radgroupreply`
+--
+ALTER TABLE `radgroupreply`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `groupname` (`groupname`(32));
+
+--
+-- Indexes for table `radpostauth`
+--
+ALTER TABLE `radpostauth`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `username` (`username`),
+  ADD KEY `class` (`class`);
+
+--
+-- Indexes for table `radreply`
+--
+ALTER TABLE `radreply`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `username` (`username`(32));
+
+--
+-- Indexes for table `radusergroup`
+--
+ALTER TABLE `radusergroup`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `username` (`username`(32));
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin_users`
+--
+ALTER TABLE `admin_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `nas`
+--
+ALTER TABLE `nas`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `radacct`
+--
+ALTER TABLE `radacct`
+  MODIFY `radacctid` bigint(21) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `radcheck`
+--
+ALTER TABLE `radcheck`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `radgroupcheck`
+--
+ALTER TABLE `radgroupcheck`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `radgroupreply`
+--
+ALTER TABLE `radgroupreply`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `radpostauth`
+--
+ALTER TABLE `radpostauth`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `radreply`
+--
+ALTER TABLE `radreply`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `radusergroup`
+--
+ALTER TABLE `radusergroup`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
